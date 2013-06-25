6707ea35fe9fcfe94bac0afe3bc642053f46db2e
James Arthur <thruflo@gmail.com>
2013-06-25 16:00:49 +0100

    release: `0.1.2`.

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,8 @@
 
+## 0.1.2
+
+Add `method` and `path_url` to logged request attributes.
+
 ## 0.1.1
 
 Include the mako template in the manifest (and thus the sdist).
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'pyramid_raven',
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Integrate raven and raven-js with a Pyramid web application.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
